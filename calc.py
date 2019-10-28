a1b5bb8737af54a0160f0eab8bdd57fa7db9cbb9
Dexter Lien <lpwm@qq.com>
2019-10-28 10:07:14 +0800

    增加提交后的正确答案popover显示

--- calc.py
+++ calc.py
@@ -22,31 +22,32 @@ class Calc:
             conn, cursor = self.dbo.connect()
             sql = 'select answer from tiankong where id=%s'
             cursor.execute(sql, id)
-            correct_answers = cursor.fetchone()['answer'].split(' ')
+            correct_answer = cursor.fetchone()['answer']
+            correct_answers = correct_answer.split(' ')
             conn.close()
 
             if len(user_answers) != len(correct_answers):
                 # 答案数量不一致
-                return False
+                return {'id': key, 'result': False, 'correct_answer': correct_answer}
             else:
                 result = True
                 for i in range(0, len(user_answers)):
                     if user_answers[i] != correct_answers[i]:
                         result = False
-                return result
+                return {'id': key, 'result': result, 'correct_answer': correct_answer}
 
         if type_timu == 'danxuan':
             user_answers = group[0]['value']
             conn, cursor = self.dbo.connect()
             sql = 'select answer from danxuan where id=%s'
             cursor.execute(sql, id)
-            correct_answers = cursor.fetchone()['answer']
+            correct_answer = cursor.fetchone()['answer']
             conn.close()
 
-            if user_answers != correct_answers:
-                return False
+            if user_answers != correct_answer:
+                return {'id': key, 'result': False, 'correct_answer': correct_answer}
             else:
-                return True
+                return {'id': key, 'result': True, 'correct_answer': correct_answer}
 
         if type_timu == 'duoxuan':
             user_answers = []
@@ -57,31 +58,32 @@ class Calc:
             sql = 'select answer from duoxuan where id=%s'
             cursor.execute(sql, id)
             # 数据库中多选题答案格式为ABCD中间没有空格,直接使用list将其转换为数组
-            correct_answers = list(cursor.fetchone()['answer'])
+            correct_answer = cursor.fetchone()['answer']
+            correct_answers = list(correct_answer)
             conn.close()
 
             if len(user_answers) != len(correct_answers):
                 # 答案数量不一致
-                return False
+                return {'id': key, 'result': False, 'correct_answer': correct_answer}
             else:
                 result = True
                 for i in range(0, len(user_answers)):
                     if user_answers[i] != correct_answers[i]:
                         result = False
-                return result
+                return {'id': key, 'result': result, 'correct_answer': correct_answer}
 
         if type_timu == 'panduan':
             user_answers = group[0]['value']
             conn, cursor = self.dbo.connect()
             sql = 'select answer from panduan where id=%s'
             cursor.execute(sql, id)
-            correct_answers = cursor.fetchone()['answer']
+            correct_answer = cursor.fetchone()['answer']
             conn.close()
 
-            if user_answers != correct_answers:
-                return False
+            if user_answers != correct_answer:
+                return {'id': key, 'result': False, 'correct_answer': correct_answer}
             else:
-                return True
+                return {'id': key, 'result': True, 'correct_answer': correct_answer}
 
     def calc_score(self, answers):
         """计算成绩
@@ -93,13 +95,10 @@ class Calc:
         detail_result = []
         sorted_data = sorted(answers, key=itemgetter('name'))  # 先进行排序
         for key, group in itertools.groupby(sorted_data, key=lambda x: x['name']):
-            if self.calc_single(key, group):
-                result = {'id': key, 'result': True}
+            single_result = self.calc_single(key, group)
+            if single_result['result']:
                 total_score += 1
-                detail_result.append(result)
-            else:
-                result = {'id': key, 'result': False}
-                detail_result.append(result)
+            detail_result.append(single_result)
 
         return_data = {'total_score': total_score, 'detail': detail_result}
         return return_data
